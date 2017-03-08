c55ccc723b3826c2335607a5870fff0f694b523d
simonet1981 <simonet1981@gmail.com>
2017-03-08 09:30:47 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -42,7 +42,7 @@ choco install openvpn
 choco install postman
 choco install soapui
 choco install resharper-platform
-choco install snaketail
+choco install baretail
  
 choco install paint.net 
 choco install skype 
