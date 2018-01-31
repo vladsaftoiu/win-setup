36450542d49193ff7e893d3519e21df615878ce6
Vlad Saftoiu Alexandru <vlad.saftoiu@live.com>
2018-01-31 17:08:58 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,51 +1,52 @@
 # Copy and paste in IE
 # http://boxstarter.org/package/url?https://https://raw.githubusercontent.com/simonet1981/win-setup/master/setup.ps1
 
-$Boxstarter.RebootOk=$true      # Allow reboots?
+#$Boxstarter.RebootOk=$true      # Allow reboots?
 
-Update-ExecutionPolicy RemoteSigned
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+#Update-ExecutionPolicy RemoteSigned
+#Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions -EnableShowFullPathInTitleBar
 
 # Dev Tools
-cinst python
-cinst conemu
-cinst git --params="/GitAndUnixToolsOnPath"
-cinst git-credential-winstore
-cinst poshgit
-cinst smartgit
-cinst winmerge
-cinst jdk8
+#cinst python -y
+#cinst conemu -y
+#cinst git --params="/GitAndUnixToolsOnPath" -y
+#cinst git-credential-winstore -y
+#cinst poshgit -y
+#cinst smartgit -y
+cinst winmerge -y
+cinst jdk8 -y
 
-cinst nuget.commandline
-cinst nugetpackageexplorer
+cinst nuget.commandline -y
+cinst nugetpackageexplorer -y
 
-cinst scriptcs  # Write .Net apps with a text editor, NuGet, and the power of Roslyn!
-cinst cshell    # CShell is an interactive C# scripting environment.
-cinst linqpad
+cinst scriptcs -y  # Write .Net apps with a text editor, NuGet, and the power of Roslyn!
+cinst cshell -y    # CShell is an interactive C# scripting environment.
+cinst linqpad -y
 
-choco install fiddler4
-choco install 7zip
+cinst fiddler4 -y
+cinst 7zip -y
 
 # Productivity
-cinst putty # checkout kitty or superputty too :)
-cinst winscp
-cinst onedrive
-choco install googlechrome
-choco install notepadplusplus
-choco install mremoteng
-choco install redis-desktop-manager 
-choco install openvpn
-choco install postman
-choco install resharper-platform
-choco install baretail
+cinst putty -y # checkout kitty or superputty too :)
+cinst winscp -y
+#cinst onedrive 
+#cinst googlechrome
+cinst notepadplusplus -y
+cinst mremoteng -y
+cinst redis-desktop-manager -y
+cinst openvpn -y
+cinst postman -y
+cinst resharper-platform -y
+cinst baretail -y
  
-choco install paint.net 
-choco install skype 
-choco install slack
-cinst telegram
+cinst paint.net -y
+cinst skype -y
+#cinst slack
+cinst telegram -y
+cinst miktex -y
 
 # Install chocolatey
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+#iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #MANUAL install
 # vistual studio 2017 
